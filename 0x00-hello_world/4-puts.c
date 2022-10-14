ba387016f5234cd0dfa9ec27056c85360c70539d
Moustapha Seck <seckmoustapha813@gmail.com>
2022-10-13 18:18:21 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <Stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
