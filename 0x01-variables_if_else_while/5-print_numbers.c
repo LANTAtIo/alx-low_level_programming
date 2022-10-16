e87e3878db9b331715188de8c58126f5a1b6ee16
Moustapha Seck <seckmoustapha813@gmail.com>
2022-10-15 18:23:45 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - numbers of base 10
+ *
+ * Return: Always 0 (Success))
+ */
+int main(void)
+
+{
+	int bas;
+
+	for (bas = 0; bas < 10; bas++)
+		printf("%d", bas);
+	printf("\n");
+
+	return (0);
+
+}
