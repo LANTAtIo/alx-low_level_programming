a1b94860cee7acfd452c4c052d501c9a71246204
Moustapha Seck <seckmoustapha813@gmail.com>
2022-10-16 19:58:26 -0700

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - reverse alphabets
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a' ; --ch)
+	{
+		putchar(ch);
+	}
+	return (0);
+
+}
 
