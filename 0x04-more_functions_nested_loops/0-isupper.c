0d43b4fb944dce500d5c9c594f646814fe6b82da
Moustapha Seck <seckmoustapha813@gmail.com>
2022-10-20 08:47:52 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,14 @@
  * _isuper upper case alphas
  * Return : Always 0
  */
-int _isupper(int c)
+int main(void)
 {
-	return (c >= 'A' && c <= 'Z');
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	
+	return (0);
 }
