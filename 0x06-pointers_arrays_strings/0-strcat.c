ba0e91da0f2233f05c7b0fdc3be90c873f0602ac
Moustapha Seck <seckmoustapha813@gmail.com>
2022-10-27 15:53:12 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * unction appends the src string to the dest string
+ * Return! Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *dest[];
+	char *src[];
+
+	strcat(dest, src);
+
+	printf("%s", dest);
+}
